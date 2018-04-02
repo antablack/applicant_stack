c5bdcbb24adca64517421e089f785f7a873c1397
Juan Sanchez <juan.sanchez@ultertechnologies.com>
2018-04-02 16:20:20 -0500

    add methos

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ var urlencodedparser = bodyParser.urlencoded({extended: false});
 app.listen(3500, function () {
     console.log("runnig aplication in port: " + 3500);
     app.use(urlencodedparser);
-    app.get("/applicant_stack", aplication_stack);
+    app.get("/applicant_stack",(req, res, next)=>{
+        console.log("GET /applicant_stack");
+        next();
+    }, aplication_stack);
+    app.post("/applicant_stack",(req, res, next)=>{
+        console.log("POST /applicant_stack");
+        next();
+    }, aplication_stack);
 })
 
 
