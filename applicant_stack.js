d7e864c5b81ac5e9df55fc72e0e4819a10b05d5e
Juan Sanchez <juan.sanchez@ultertechnologies.com>
2018-04-03 10:32:07 -0500

    api support

--- applicant_stack.js
+++ applicant_stack.js
@@ -1,10 +1,34 @@
 const TOKEN = "2uyau0e0y8i97f832e0m74d6lv003ovgkrv0imvx";
-const URL = "https://tinphongsu.applicantstack.com";
+const URL = "https://";
 const KEYSECRET = "fhqiieqy1dq8h0fe";
-
+const https = require("https");
 function receiveNotification(req, res) {
+    console.log("---------------------------------------")
     console.log(req.headers);
     console.log(req.body);
+
+    var idCandidate = "a4r6kxcdwmrc";
+    let URLCandidate = `/api/candidate/${req.body["Candidate Serial"]}`;
+    let URLCandidates = `/api/candidates`;
+
+    let options = {
+        host: "tinphongsu.applicantstack.com",
+        path: URLCandidate,
+        headers: {
+            "Token": TOKEN
+        }
+    }
+    console.log("getting " + URLCandidates)
+    https.get(options, (res) => {
+        res.setEncoding("utf8");
+        let body = "";
+        res.on("data", data => {
+            body += data;
+        });
+        res.on("end", (a) => {
+            console.log(body);
+        });
+    })
     res.sendStatus(200);
 }
 
